ae9375741c54ff752f4c87bce9ced1d4b9817539
Simon Jakobsen <Simon_Jakobsen@Gravio.dk>
2021-07-12 11:36:59 +0200

    Persisting Authentication Status Across Page Refreshes

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { AuthService } from './user/auth.service';
 
 @Component({
     selector: 'events-app',
@@ -9,6 +10,10 @@ import { Component } from '@angular/core'
     `
 })
 
-export class EventsAppComponent{
+export class EventsAppComponent {
+    constructor(private auth: AuthService) { }
 
+    ngOnInit() {
+        this.auth.checkAuthenticationStatus();
+    }
 }
